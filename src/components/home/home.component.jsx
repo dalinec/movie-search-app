bfa4a8ff4e5a4551dab627b108d623062f2ded4a
Matej Kukolik <dalinec123@gmail.com>
2022-10-10 13:05:52 +0200

    add home component and movie fetch functionality

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,10 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import MovieListing from '../movie-listing/movie-listing.component';
+import movieApi from '../../common/apis/movieApi';
 
 const Home = () => {
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_OMDB_API_KEY;
+    const movieText = 'Harry';
+    const fetchMovies = async () => {
+      const res = await movieApi
+        .get(`?apiKey=${apiKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log('Err:', err);
+        });
+      console.log('The response from the API', res);
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <>
-      <Outlet />
-      <div>Home</div>
+      <div className='banner-img'></div>
+      <MovieListing />
     </>
   );
 };
