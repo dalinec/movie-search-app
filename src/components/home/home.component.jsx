16efb7cca73991890bd0216433cb8636a8cb2d7e
Matej Kukolik <dalinec123@gmail.com>
2022-10-10 13:28:17 +0200

    add movieSlice

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,18 +1,22 @@
 import { useEffect } from 'react';
 import MovieListing from '../movie-listing/movie-listing.component';
 import movieApi from '../../common/apis/movieApi';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/moviesSlice';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const movieText = 'Harry';
+
   useEffect(() => {
     const apiKey = process.env.REACT_APP_OMDB_API_KEY;
-    const movieText = 'Harry';
     const fetchMovies = async () => {
       const res = await movieApi
         .get(`?apiKey=${apiKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log('Err:', err);
         });
-      console.log('The response from the API', res);
+      dispatch(addMovies(res.data));
     };
     fetchMovies();
   }, []);
