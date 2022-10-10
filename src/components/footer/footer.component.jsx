424c85bc74c20f4281b2f05f71fdb787be18b8b0
Matej Kukolik <dalinec123@gmail.com>
2022-10-10 12:05:41 +0200

    add footer styling

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -1,5 +1,12 @@
+import './footer.style.scss';
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className='footer'>
+      <div>Movie App</div>
+      <div>©2022, Movie, Inc. or its affiliates</div>
+    </div>
+  );
 };
 
 export default Footer;
