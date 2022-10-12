221f9ac2f3125af0fadc52d6a8ee88a0d03e2000
Matej Kukolik <dalinec123@gmail.com>
2022-10-12 13:02:41 +0200

    add carousel

--- src/components/movie-listing/movie-listing.component.jsx
+++ src/components/movie-listing/movie-listing.component.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { getAllMovies, getAllShows } from '../../features/movies/moviesSlice';
 
+import Slider from 'react-slick';
+import { Settings } from '../../common/settings';
+
 import MovieCard from '../movie-card/movie-card.component';
 
 import './movie-listing.style.scss';
 
 const MovieListing = () => {
+  const settings = Settings;
   const movies = useSelector(getAllMovies);
   const shows = useSelector(getAllShows);
   console.log(movies);
@@ -36,11 +40,15 @@ const MovieListing = () => {
     <div className='movie-wrapper'>
       <div className='movie-list'>
         <h2>Movies</h2>
-        <div className='movie-container'>{renderMovies}</div>
+        <div className='movie-container'>
+          <Slider {...settings}>{renderMovies}</Slider>
+        </div>
       </div>
       <div className='movie-list'>
         <h2>Shows</h2>
-        <div className='movie-container'>{renderShows}</div>
+        <div className='movie-container'>
+          <Slider {...settings}>{renderShows}</Slider>
+        </div>
       </div>
     </div>
   );
